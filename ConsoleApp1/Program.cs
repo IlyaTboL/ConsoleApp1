a3b5e57c810d1cb8d8f8ec05cecbf69cbf122b77
Илья Корытов <t.b.o.l@mail.ru>
2022-10-01 17:51:51 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class MainClass {
+    public static void Main
+        (string[] args) {
+
+        string myName;
+        myName = "Илья";
+        Console.WriteLine(myName);
+        Console.ReadKey();
+    }
+}

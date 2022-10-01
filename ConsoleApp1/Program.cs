305f160e67bf95bf04f526ef264c8ebad3c76df9
Илья Корытов <t.b.o.l@mail.ru>
2022-10-01 18:30:08 +0400

    анкетка

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,9 +3,16 @@ class MainClass {
     public static void Main
         (string[] args) {
 
-        string myName;
-        myName = "Илья";
-        Console.WriteLine(myName);
-        Console.ReadKey();
+        string myName = "Илья";
+        byte MyAge = 28;
+        bool HaveIApet = true;
+        string HimName = "JoJo";
+            double MyShoesSize = 44.5;
+        Console.WriteLine("My name is " + myName);
+        Console.WriteLine("My Age " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("What is his name? " + HimName);
+        Console.WriteLine("My shoe size is " + MyShoesSize);
+        
     }
 }

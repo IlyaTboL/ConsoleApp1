fe3007e8bca7556b8b1b168d07d544f419f12fe6
Илья Корытов <t.b.o.l@mail.ru>
2022-10-04 23:32:56 +0400

    анкета (Form)

---
+++ Form/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            var age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine($"Your name is {name} and age is {age} ");
+
+            Console.Write("Enter your birthdate: ");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine($"Your birthdate is {birthdate}");
+            Console.ReadKey();
+        }
+    }

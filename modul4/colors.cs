0fa9b616368a08d8040ece17a18f76a28ee1671d
Илья Корытов <t.b.o.l@mail.ru>
2022-10-13 02:27:12 +0400

    условные выражения. цвета

---
+++ modul4/colors.cs
@@ -0,0 +1,32 @@
+﻿class MainClass
+{
+    public static void Main(string[]agrs)
+    {
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+        var color = Console.ReadLine();
+
+        if (color == "red")
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is red!");
+        }
+
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("your color is green!");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is cyan");
+        }
+    }
+}

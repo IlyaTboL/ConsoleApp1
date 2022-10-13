c63499de14abf96d4098285aebb6a235d9aca005
Илья Корытов <t.b.o.l@mail.ru>
2022-10-13 20:12:32 +0400

    кортеж

---
+++ anketa/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        (string name, int age) anketa;
+
+        Console.Write("Введите имя: ");
+        anketa.name = Console.ReadLine();
+        Console.WriteLine("Введите возраст с цифрами: ");
+        anketa.age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ваше имя: {0}", anketa.name);
+        Console.WriteLine("Ваш возраст {0}", anketa.age);
+
+        Console.ReadKey();
+    }
+}

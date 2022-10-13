5d43573c8b8a9a35754f597bded2012e3ab1491e
Илья Корытов <t.b.o.l@mail.ru>
2022-10-13 04:21:26 +0400

    имя наооборот

--- massiv/masiv.cs
+++ massiv/masiv.cs
@@ -6,14 +6,10 @@
 
         var name = Console.ReadLine();
 
-        Console.WriteLine("Ваше имя по буквам: ");
-
-        foreach( var ch in name)
+       for ( int i = name.Length - 1; i >= 0; i--)
         {
-            Console.WriteLine(ch + " ");
+            Console.WriteLine(name[i] + "");
         }
-
-        Console.WriteLine("Последняя буква вашего имени:{0}", name[name.Length - 1]);
         
         Console.ReadKey();
     }
